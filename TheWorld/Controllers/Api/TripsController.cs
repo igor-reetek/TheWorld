4999d918a3696ec5a7dafdc9378d0cafe5f58fb3
igormeskoski <imeskoski@reetek.com.au>
2017-10-23 15:47:51 +1100

    Add view models and validation

--- TheWorld/Controllers/Api/TripsController.cs
+++ TheWorld/Controllers/Api/TripsController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TheWorld.Models;
+using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Api
 {
@@ -27,9 +28,14 @@ namespace TheWorld.Controllers.Api
         }
 
         [HttpPost("")]
-        public IActionResult Post([FromBody]Trip trip)
+        public IActionResult Post([FromBody]TripViewModel trip)
         {
-            return Ok(true);
+            if (ModelState.IsValid)
+            {
+                return Created($"api/trips/{trip.Name}", trip);
+            }
+
+            return BadRequest(ModelState);
         }
     }
 }
